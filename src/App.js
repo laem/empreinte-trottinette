1a8acca40d3445e4ed5cfda6a9608f2fe11bf832
Mael <mael.thomas@data.gouv.fr>
2020-09-30 12:35:49 +0200

    Illustration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ecotrain from './ecotrain sur un pont.png'
+import illustration from './trottinette.svg'
 import './App.css'
 // On définit une liste de règles publicodes
 import { résultat, engine } from './Calcul.js'
@@ -17,27 +17,40 @@ function App() {
 			css={`
 				margin: 0 auto;
 				max-width: 800px;
+				h1 {
+					margin-bottom: 2rem;
+				}
 			`}
 		>
 			<Router>
 				<header>
 					<h1>Trottinette partagée - empreinte carbone</h1>
-					<img src={'/' + ecotrain} css="width: 20rem" />
 				</header>
-				<div>
-					<p>
-						L'empreinte totale d'une trottinette partagée est estimée à
-						<strong>{résultat}</strong>
-					</p>
-					<p>
-						Cet excellente estimation tranche avec tous les autres modes de
-						transport qui sont largement plus polluants, excepté le TGV qui
-						obtient un résultat similaire.{' '}
-					</p>
-					<p>
-						Nous publions donc ci-dessous le calcul dans son intégralité pour
-						que chacun puisse le comprendre et le remettre en question.
-					</p>
+				<div
+					css={`
+						img {
+							max-height: 10rem;
+						}
+						display: flex;
+						justify-content: center;
+					`}
+				>
+					<img src={'/' + illustration} css="width: 20rem" />
+					<div>
+						<p>
+							L'empreinte totale d'une trottinette partagée est estimée à
+							<strong>{résultat}</strong>
+						</p>
+						<p>
+							Cet excellente estimation tranche avec tous les autres modes de
+							transport qui sont largement plus polluants, excepté le TGV qui
+							obtient un résultat similaire.{' '}
+						</p>
+						<p>
+							Nous publions donc ci-dessous le calcul dans son intégralité pour
+							que chacun puisse le comprendre et le remettre en question.
+						</p>
+					</div>
 				</div>
 				<h2>Explication du calcul</h2>
 				<Switch>
